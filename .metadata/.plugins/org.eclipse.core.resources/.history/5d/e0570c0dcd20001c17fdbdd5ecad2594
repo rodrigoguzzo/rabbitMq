d6a2379d60cf0cd631e23f65826de9f2d857b36b
Sieczko Guzzo <reso@gft.com>
2021-09-28 23:31:17 -0300

    ataulizacao

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/5d/e0570c0dcd20001c17fdbdd5ecad2594
@@ -0,0 +1,5 @@
+package com.example.demo.envio;
+
+public class OrderQueueSender {
+
+}
--- RabbitProject/src/main/java/com/example/demo/consumo/OrderConsumer.java
+++ RabbitProject/src/main/java/com/example/demo/consumo/OrderConsumer.java
@@ -1,5 +1,22 @@
 package com.example.demo.consumo;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderConsumer {
 
+	/*
+	 * Por padrão, a anotação @RabbitListener instância apenas um consumer, 
+	 * ou seja, não trabalha com concorrência na leitura da fila, 
+	 * caso seja necessário mudar esse comportamento, configurar as 
+	 * propriedades spring.rabbitmq.listener.simple.concurrency e 
+	 * spring.rabbitmq.listener.simple.max-concurrency 
+	 * no application.properties.
+	 */
+	@RabbitListener(queues = {"${queue.order.name"})
+	public void receive(@Payload String fileBody) {
+		System.out.println("Order : " + fileBody);
+	}
 }
